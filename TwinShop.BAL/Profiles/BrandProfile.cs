c2dd69425c77eef35238ac8b9438f45526847ef0
Shayan-Shams-2005 <shayanshams621@gmail.com>
2026-02-24 17:57:08 +0330

    added autoMapper Profile to project

---
+++ TwinShop.BAL/Profiles/BrandProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Twin_Shop__Web_API.DTOs.Brand;
+using Twin_Shop__Web_API.Entities;
+
+namespace TwinShop.BLL.Profiles
+{
+    public class BrandProfile:Profile
+    {
+        public BrandProfile()
+        {
+            CreateMap<Brand, BrandDto>();       
+            CreateMap<CreateBrandDto, Brand>();  
+        }
+    }
+}
